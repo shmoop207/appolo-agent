8d79ff58384f5bf7f0ac6e5986e183c1fe186998
roman <shmoop207@gmail.com>
2018-04-13 20:59:19 +0300

    first

--- lib/IApp.ts
+++ lib/IApp.ts
@@ -1,5 +1,8 @@
 import {IResponse} from "./response";
+import {IOptions} from "./IOptions";
 
 export interface IApp {
-    render(path: string | string[], params?: any,res?:IResponse): Promise<string>
+    render(path: string | string[], params?: any, res?: IResponse): Promise<string>
+
+    options: IOptions
 }
